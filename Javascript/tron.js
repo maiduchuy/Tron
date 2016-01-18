53c2a4e2d888dcd9d199249b7a89f94b812ec3ff
MDcH <huyquyenquyen@gmail.com>
2016-01-18 16:51:17 +0700

    Refactoring
    Renaming, Refactoring, Re-implement code

--- Javascript/tron.js
+++ Javascript/tron.js
@@ -1,33 +1,61 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert, clickOutsideHideSearchBar*/
 
-function showHideSearchBar() {
+function toggleSearchBar() {
   "use strict";
-  if ($("#search-wrapper").css("z-index") !== "0") {
-    $("#search-wrapper").css("z-index", "0");
+
+  var duration = 150,
+    d1,
+    d2;
+
+  if ($("#search-field").hasClass("show")) {
+    $("#search-field").removeClass('show');
+    $("#search-form").css("z-index", "-1");
+
+    $(".nav-btn, #account-btn").stop();
+    $("#search-field").stop();
+
+    d1 = $("#search-field").width() / $("#search-field").parent().width() * duration;
+    d2 = d1 / 3;
+
+    $(".nav-btn, #account-btn").fadeIn(d2, "easeInExpo");
+    $("#search-field").animate({
+      width: 'toggle',
+      opacity: 'toggle'
+    }, d1, "easeOutSine");
+
   } else {
-    $("#search-wrapper").css("z-index", "-1");
+    $("#search-field").addClass('show');
+    $("#search-form").css("z-index", "0");
+
+    $(".nav-btn, #account-btn").stop();
+    $("#search-field").stop();
+
+    d1 = (1 - $("#search-field").width() / $("#search-field").parent().width()) * duration;
+    d1 = ($("#search-field").is(":visible")) ? d1 : duration;
+    d2 = d1 / 3;
+
+    $(".nav-btn, #account-btn").fadeOut(d2, "easeOutExpo");
+    $("#search-field").animate({
+      width: 'toggle',
+      opacity: 'toggle'
+    }, d1, "easeInSine", function () {
+      this.focus();
+    });
   }
-  $(".nav-btn, .nav-item .account-icon").fadeToggle(50);
-  $("#search-field").animate({
-    width: 'toggle',
-    opacity: 'toggle'
-  }, 150, function () {
-    this.focus();
-  });
 }
 
 function clickOutsideHideSearchBar() {
   "use strict";
   if (!$("a").is(event.target) && !$("a").has(event.target).length && $("#search-field").is(":visible") && !$("#search-field").is(event.target)) {
-    showHideSearchBar();
+    toggleSearchBar();
   }
 }
 
 $(function () {
   "use strict";
 
-  $("#search-btn").on("click", showHideSearchBar).on("click", function (event) {
+  $("#search-btn").on("click", toggleSearchBar).on("click", function (event) {
     event.preventDefault();
   });
 
